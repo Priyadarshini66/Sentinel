8986b8372dc5538bb227385e721192be081e4f44
Priyadarshini66 <priyaraj66.r@gmail.com>
2023-07-31 12:43:59 +0530

    1

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 resource "aws_lb" "test" {
-  name               = "test-lb-tf"
+name               = "test-lb-tf"
   internal           = false
+  load_balancer_type = "application"
+ 
+
+  enable_deletion_protection = true
+
+  
  
 }
